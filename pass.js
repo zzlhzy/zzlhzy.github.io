1173ff5ff6355cea1daa7b1edcac6fdc4ff39752
zzlhzy <zxcvbnm81606807@163.com>
2022-12-31 22:23:20 +0800

    a

--- pass.js
+++ pass.js
@@ -1,10 +1,12 @@
-$("#idBtn").click(function(){
-	//获取值两个
-	var password = $("#password").val();
-	var pwd=$("#oldPwd").val();
-	if(pwd == password){
-		layer.msg("一致");
-	}else{
-		layer.msg("两次密码不一致");
-	}
+validation.addEventListener('click', () => {
+    let validation = document.getElementById('validation');
+    let psdJudge = document.getElementById('psdJudge');
+    let length = psd.value.length;
+    if (length >= 6 && length <= 16) {
+        psdJudge.innerHTML = '输入正确信息'
+        setTimeout(1000)
+        window.open("zzlhzy.github.io/hzy.html");
+    }else {
+        psdJudge.innerHTML = '错误信息';
+    }
 })
